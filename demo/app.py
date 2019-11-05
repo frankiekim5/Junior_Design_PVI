3d9a7e50700de0a6100d0852da5cabe5adb9080b
Max Zuo <max.zuo@gmail.com>
2019-11-04 19:44:09 -0500

    made boilerplate code for all the main screens

--- demo/app.py
+++ demo/app.py
@@ -12,9 +12,26 @@ def main():
 	return render_template("index.html")
 
 
-@app.route('/')
+@app.route('/login')
 def login():
 	return render_template("index.html")
+
+@app.route('/home')
+def home():
+	return render_template("home.html")
+
+
+@app.route('/pantry')
+def pantry():
+	return render_template("pantry.html")
+
+@app.route('/meals')
+def meals():
+	return render_template("meals.html")
+
+@app.route('/settings')
+def settings():
+	return render_template("settings.html")
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', threaded=True, debug=True)
