4ae0ab0d31bb364dbe133217413037a254035421
Max Zuo <max.zuo@gmail.com>
2019-11-04 20:57:29 -0500

    Implemented basic view stack (back button) to work. Still needs to be able to change screens though

--- demo/app.py
+++ demo/app.py
@@ -5,7 +5,7 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def main():
 	if request.method == "POST":
 		return render_template("index.html")
--- demo/static/index.js
+++ demo/static/index.js
@@ -1,3 +1,46 @@
 let viewStack = []
+const taskbars = ["Health", "Pantry", "Meals", "Settings"]
 
+let taskbarClick = (elem) => {
+	// change selected taskbar button
+	$(".taskBarButton").removeClass("selectedTBI")
+	$(elem).addClass("selectedTBI")
 
+	// make back button visible
+	$("#backButton").css('visibility', 'visible')
+	
+	// Change title text
+	const title = $(elem).attr("name")
+	viewStack.push(title)	// In the future, we should make this into an object, {title: , screen: }
+	$("#title").text(title)
+	// TODO: Actually show the page
+	// switch (elem.id) {}
+}
+
+$(document).ready(() => {
+	$("#backButton").click(() => {
+		console.log("WHAT?")
+		const currentView = viewStack.pop()
+		
+		if (viewStack.length == 0) {
+			$("#title").text("PVI")
+			$(".taskBarButton").removeClass("selectedTBI")
+			$("#backButton").css('visibility', 'hidden')
+			return
+		} else if (taskbars.includes(currentView)) {
+			let last_task = "PVI" //This logic might break
+			for (let i = viewStack.length - 1; i >= 0; i--) {
+				if (taskbars.includes(viewStack[i])) {
+					last_task = viewStack[i]
+					break
+				}
+			}
+			$('.taskBarButton').removeClass("selectedTBI")
+			$('#' + last_task.toLowerCase() + "Button").addClass("selectedTBI")
+			$("#title").text(viewStack[viewStack.length - 1])
+
+		} else {
+			$("#title").text(viewStack[viewStack.length - 1])
+		}
+	})
+})
