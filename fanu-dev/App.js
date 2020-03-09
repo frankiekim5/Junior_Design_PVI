54fdf47d4ff6f8cde4c8b625e939862c1badfdae
evisalguero <evisalguero@gmail.com>
2020-03-09 17:22:58 -0400

    navigation - login

--- fanu-dev/App.js
+++ fanu-dev/App.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import 'react-native-gesture-handler';
 import {
   // SafeAreaView,
   // StyleSheet,
   // ScrollView,
-  View,
+  //View,
   // Text,
   // StatusBar,
   // ImageBackground,
@@ -23,82 +23,103 @@ import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom
 import Pie from 'react-native-pie';
 import SettingsScreen from './src/components/screens/SettingsScreen'
 import InventoryScreen from './src/components/screens/InventoryScreen'
-import HomeScreen from './src/components/screens/HomeScreen'
+//import HomeScreen from './src/components/screens/HomeScreen'
 import MealsScreen from './src/components/screens/MealsScreen'
 import ServerScreen from './src/components/screens/Server'
+import LoginScreen from './src/components/screens/LoginScreen'
 import RegistrationScreen from './src/components/screens/RegistrationScreen';
 
-const TabNavigator = createMaterialBottomTabNavigator(
-  {
-    Home: {
-      screen: HomeScreen,
-      navigationOptions: {
-        tabBarIcon: ({tintColor}) => (
-          <View>
-            <Icon name="home" size={25} />
-          </View>
-        ),
-      },
-    },
-    Inventory: {
-      screen: InventoryScreen,
-      navigationOptions: {
-        tabBarIcon: ({tintColor}) => (
-          <View>
-            <Icon name="local-grocery-store" size={25} />
-          </View>
-        ),
-      },
-    },
-    Meals: {
-      screen: MealsScreen,
-      navigationOptions: {
-        tabBarIcon: ({tintColor}) => (
-          <View>
-            <OIcon name="food" size={25} />
-          </View>
-        ),
-      },
-    },
-    Settings: {
-      screen: SettingsScreen,
-      navigationOptions: {
-        tabBarIcon: ({tintColor}) => (
-          <View>
-            <Icon name="settings" size={25} />
-          </View>
-        ),
-      },
-    },
-    // Registration: {
-    //   screen: RegistrationScreen,
-    //   navigationOptions: {
-    //     tabBarIcon: ({tintColor}) => (
-    //       <View>
-    //         <Icon name="settings" size={25} />
-    //       </View>
-    //     ),
-    //   },
-      
-      
-    // }
-    Server: {
-      screen: ServerScreen,
-      navigationOptions: {
-        tabBarIcon: ({tintColor}) => (
-          <View>
-            <Icon name="developer-mode" size={25} />
-          </View>
-        ),
-      },
-    },
-  },
-  {
-    initialRouteName: 'Home',
-    activeColor: '#f0edf6',
-    inactiveColor: '#3e2465',
-    barStyle: {backgroundColor: '#71eeb8'},
-  },
-);
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default createAppContainer(TabNavigator);
+//<Stack.Screen name="Home" component={HomeScreen} />
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+
+// const TabNavigator = createMaterialBottomTabNavigator(
+//   {
+//     Home: {
+//       screen: HomeScreen,
+//       navigationOptions: {
+//         tabBarIcon: ({tintColor}) => (
+//           <View>
+//             <Icon name="home" size={25} />
+//           </View>
+//         ),
+//       },
+//     },
+//     Inventory: {
+//       screen: InventoryScreen,
+//       navigationOptions: {
+//         tabBarIcon: ({tintColor}) => (
+//           <View>
+//             <Icon name="local-grocery-store" size={25} />
+//           </View>
+//         ),
+//       },
+//     },
+//     Meals: {
+//       screen: MealsScreen,
+//       navigationOptions: {
+//         tabBarIcon: ({tintColor}) => (
+//           <View>
+//             <OIcon name="food" size={25} />
+//           </View>
+//         ),
+//       },
+//     },
+//     Settings: {
+//       screen: SettingsScreen,
+//       navigationOptions: {
+//         tabBarIcon: ({tintColor}) => (
+//           <View>
+//             <Icon name="settings" size={25} />
+//           </View>
+//         ),
+//       },
+//     },
+//     // Registration: {
+//     //   screen: RegistrationScreen,
+//     //   navigationOptions: {
+//     //     tabBarIcon: ({tintColor}) => (
+//     //       <View>
+//     //         <Icon name="settings" size={25} />
+//     //       </View>
+//     //     ),
+//     //   },
+      
+      
+//     // }
+//     Server: {
+//       screen: ServerScreen,
+//       navigationOptions: {
+//         tabBarIcon: ({tintColor}) => (
+//           <View>
+//             <Icon name="developer-mode" size={25} />
+//           </View>
+//         ),
+//       },
+//     },
+//   },
+//   {
+//     initialRouteName: 'Home',
+//     activeColor: '#f0edf6',
+//     inactiveColor: '#3e2465',
+//     barStyle: {backgroundColor: '#71eeb8'},
+//   },
+// );
